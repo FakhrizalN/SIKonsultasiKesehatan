9af5be52303a90f2410f2b2d42d126780b82d228
Hylmi220903 <11221023@student.itk.ac.id>
2024-11-04 09:12:15 +0800

    merubah cardinfo.vue

--- tugbes/src/components/CardInfo.vue
+++ tugbes/src/components/CardInfo.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="container">
-        <img class="icon" :src="require(`@/assets/${icon}.svg`)" alt="Icon" />
-        <div class="text-container">
-            <div class="heading">{{ heading }}</div>
-            <div class="subtext">{{ subtext }}</div>
+    <div class="container bg-white rounded-lg shadow-lg flex flex-col items-center gap-4 p-6 w-[304px]">
+        <img class="icon w-[70px] h-[70px]" :src="require(`@/assets/${icon}.svg`)" alt="Icon" />
+        <div class="text-container flex flex-col items-center gap-2 w-[256px]">
+            <div class="heading text-[#011632] font-medium text-xl leading-[28.8px] text-center">{{ heading }}</div>
+            <div class="subtext text-[#011632] font-light text-lg leading-[27.9px] tracking-[0.015em] text-center">{{ subtext }}</div>
         </div>
     </div>
 </template>
@@ -27,56 +27,3 @@ export default {
     }
 };
 </script>
-
-<style scoped>
-    .container {
-        align-items: center;
-        background-color: #ffffff;
-        border-radius: 10px;
-        box-shadow: 0px 0px 0px 1px #0000000F;
-        box-shadow: 0px 10px 36px 0px #00000029;
-        display: flex;
-        flex-direction: column;
-        gap: 16px;
-        /* overflow: hidden; */
-        padding: 24px;
-        position: relative;
-        width: 304px;
-    }
-
-    .icon {
-        height: 70px;
-        position: relative;
-        width: 70px;
-    }
-
-    .text-container {
-        align-items: center;
-        align-self: stretch;
-        display: flex;
-        flex: 0 0 auto;
-        flex-direction: column;
-        gap: 8px;
-        position: relative;
-        width: 256px;
-    }
-
-    .heading {
-        align-self: stretch;
-        color:#011632;
-        font-family: "General Sans";
-        font-size: 24px;
-        font-weight: 500;
-        line-height: 28.8px;
-        text-align: center;
-
-    }
-    .subtext {
-        font-family: "General Sans";
-        font-size: 18px;
-        font-weight: 300;
-        line-height: 27.9px;
-        letter-spacing: 0.015em;
-        text-align: center;
-    }
-</style>
