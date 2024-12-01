295f6fc75f384c5e2881b45ae2527a1510882a6c
Idin16 <155719060+Idin16@users.noreply.github.com>
2024-12-01 22:23:26 +0800

    Perbaikan Ask Doctor

--- tugbes/src/components/DoctorCardComponent.vue
+++ tugbes/src/components/DoctorCardComponent.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< Updated upstream
     <div class="bg-white p-4 rounded-lg shadow flex items-center space-x-4 w-full">
       <img :src="image" alt="Doctor Image" class="w-16 h-16 rounded-full object-cover" />
       <div class="flex flex-col flex-grow">
@@ -13,10 +14,22 @@
             <i class="fas fa-briefcase text-gray-500"></i>
             <span class="text-sm text-gray-600">{{ experience }} Years</span>
           </div>
+=======
+  <div class="w-[392px] h-[188px] px-6 py-4 bg-white flex-col justify-start items-start gap-2 inline-flex">
+    <div class="self-stretch h-[156px] flex flex-col justify-start items-start gap-2">
+      <div class="self-stretch flex justify-start items-center gap-4">
+        <div class="rounded-[86px] shadow flex justify-start items-center gap-2.5">
+          <img :src="require(`@/assets/${image}`)" alt="Doctor Image" class="w-[97px] h-[97px]" />
+        </div>
+        <div class="flex-grow flex flex-col justify-start items-start gap-2">
+          <div class="self-stretch text-[#011632] text-2xl font-normal capitalize leading-[28.80px]">{{ name }}</div>
+          <div class="self-stretch text-[#3c4959] text-lg font-normal leading-7 tracking-tight">{{ specialty }}</div>
+>>>>>>> Stashed changes
         </div>
       </div>
       <button class="bg-blue-500 text-white px-4 py-2 rounded-lg font-semibold text-sm">Chat</button>
     </div>
+<<<<<<< Updated upstream
   </template>
   
   <script>
@@ -31,3 +44,26 @@
   };
   </script>
   
+=======
+  </div>
+</template>
+
+<script>
+import Button from './Button.vue';
+export default {
+  components: {
+    Button,
+  },
+  props: {
+    name: String,
+    specialty: String,
+    rating: Number,
+    experience: Number,
+    image: {
+      type: String,
+      require: true
+    },
+  },
+};
+</script>
+>>>>>>> Stashed changes
